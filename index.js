fc2bd084d56c7d55f8c3b2f90301435c491d924b
Wes <wesley.has@outlook.com>
2023-09-17 09:02:14 -0600

    added proper size to shapes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const shapes = require('./lib/shapes');
+const generateShapes = require('./lib/generateShapes');
 const inquirer = require('inquirer');
 
 // Define an array of questions for the user
@@ -27,9 +27,31 @@ const questions = [
     },
   ];
 
-  function init() {
-    inquirer.prompt(questions)
-  };
+  // function init() {
+  //   inquirer.prompt(questions)
+  // };
 
+  // Function to write data to a file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, err => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('SVG created.');
+    }
+  })
+};
+
+// Function to initialize the application
+function init() {
+  inquirer.prompt(questions)
+    .then((data) => {
+      const dataShapes = generateShapes(data);
+      writeToFile('logo.svg', dataShapes);
+    })
+    .catch((err) => {
+      console.error('Something went wrong:', err.message);
+    });
+};
 
   init();
